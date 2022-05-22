210768989284248b23a8cedcfe622c63015dd4ea
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-22 12:03:40 -0300

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3506,7 +3506,9 @@ class PlayState extends MusicBeatState
 
 					if (SONG.validScore)
 					{
+					  #if newgrounds
 						NGio.unlockMedal(60961);
+						#end
 						Highscore.saveWeekScore(storyWeek, campaignScore, storyDifficulty);
 					}
 
