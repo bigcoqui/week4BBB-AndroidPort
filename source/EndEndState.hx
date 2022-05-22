bc2ef866e312c36f638c352bf489c0e74e26c791
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-22 12:08:53 -0300

    update EndEndState.hx

--- source/EndEndState.hx
+++ source/EndEndState.hx
@@ -26,6 +26,10 @@ class EndEndState extends FlxState
 		add(bg);
         //FlxG.sound.playMusic(Paths.music("eilot","shared"),1,false);
         //camera.flash(FlxColor.BLACK, 1);
+        
+  #if android
+  addVirtualPad(NONE, A);
+  #end
 	}
 	override public function update(elapsed:Float):Void 
 	{
