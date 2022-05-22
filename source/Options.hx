f850730cf3bbebf83e17aca1cc99c5f51da73d7f
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-22 17:05:59 -0300

    cu

--- source/Options.hx
+++ source/Options.hx
@@ -72,7 +72,25 @@ class Option
 	public function right():Bool { return throw "stub!"; }
 }
 
+class AndroidControls extends Option
+{
+	public function new()
+	{
+		super();
+	}
 
+	public override function press():Bool
+	{
+		trace("switch");
+		FlxG.switchState(new android.AndroidControlsMenu());
+		return false;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Android Controls";
+	}
+}
 
 class DFJKOption extends Option
 {
@@ -654,24 +672,4 @@ class BotPlay extends Option
 	
 	private override function updateDisplay():String
 		return "BotPlay " + (FlxG.save.data.botplay ? "on" : "off");
-}
-		
-class AndroidControls extends Option
-{
-	public function new()
-	{
-		super();
-	}
-
-	public override function press():Bool
-	{
-		if(changeData)
-		FlxG.switchState(new android.AndroidControlsMenu());
-		return false;
-	}
-
-	private override function updateDisplay():String
-	{
-		return "Android Controls";
-	}
 }
